b28622210f2af82afc91c077886bc05b2256241f
Gabriel Hart <gehart77@gmail.com>
2025-05-28 11:23:20 -0500

    Initial Commit

---
+++ MLB Predictions/MLB_PredictionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLB_PredictionsApp.swift
+//  MLB Predictions
+//
+//  Created by Gabe Hart on 5/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MLB_PredictionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MLB PredictionsTests/MLB_PredictionsTests.swift
@@ -0,0 +1,17 @@
+//
+//  MLB_PredictionsTests.swift
+//  MLB PredictionsTests
+//
+//  Created by Gabe Hart on 5/28/25.
+//
+
+import Testing
+@testable import MLB_Predictions
+
+struct MLB_PredictionsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
